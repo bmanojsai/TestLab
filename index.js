ed6ec703a5c9373eb4e9278e74974f6607571b68
manojsai_bellamkonda <manojsai.bellamkonda@bounteous.com>
2023-06-01 14:42:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+import TestText from "./src/components/TestText/TestText";
+
+export {TestText};
---
+++ src/components/TestText/TestText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const TestText = () => {
+  return (
+    <View><Text>This is from Testlab</Text></View>
+  )
+}
+
+export default TestText;
