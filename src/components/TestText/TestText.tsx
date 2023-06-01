4eac7683e0522f9dd375c29cdb24e45bcb3385d8
manojsai_bellamkonda <manojsai.bellamkonda@bounteous.com>
2023-06-01 17:14:16 +0530

    converted from js to ts

--- src/components/TestText/TestText.tsx
+++ src/components/TestText/TestText.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from "react-native";
 
-const TestText = () => {
+const TestText:React.FC = () => {
   return (
     <View><Text>This is from Testlab</Text></View>
   )
